4149003a49ddae8577a3f82d9ff7089b0847604d
elstonmiller <elstonmiller@usf.edu>
2026-01-27 12:34:44 -0500

    Updated annotations

--- ElstonMiller_Wk3.R
+++ ElstonMiller_Wk3.R
@@ -1,9 +1,12 @@
+# Create Vectors
 Name <- c("Jeb", "Donald", "Ted", "Marco", "Carly", "Hillary", "Berine")
 
 ABC_political_poll_results <- c(4, 62, 51, 21, 2, 14, 15)
 
 CBS_political_poll_results <- c(12, 75, 43, 19, 1, 21, 19) 
 
+# Combine into DF
 political_poll_df <- data.frame(Name, ABC_political_poll_results, CBS_political_poll_results)
 
+# Display DF
 political_poll_df
