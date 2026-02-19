f7c936eadbbd5d7e1be21ee0e2b1655954978937
elstonmiller <elstonmiller@usf.edu>
2026-02-19 03:28:47 -0500

    Completed assignment

--- ElstonMiller_Wk6.R
+++ ElstonMiller_Wk6.R
@@ -5,11 +5,14 @@ B=matrix(c(5,2,4,-1), ncol=2)
 
 ## a) Find A + B
 A + B
+
 ## b) Find A - B
 A - B
 
 # 2. Using the diag() function to build a matrix of size 4 with the following values in the diagonal 4,1,2,3.
+
 diag(c(4,1:3))
+
 # 3. Generate the following matrix:
 
   ##      [,1] [,2] [,3] [,4] [,5]
@@ -18,4 +21,8 @@ diag(c(4,1:3))
   ## [3,]  2    0     3    0    0
   ## [4,]  2    0     0    3    0
   ## [5,]  2    0     0    0    3
-  Hint: You can use the command diag() to build it.
+
+M <- matrix(rep(0,25), nrow = 5)
+M[1,] <- 1
+M[,1] <- 2
+diag(M) <- 3
